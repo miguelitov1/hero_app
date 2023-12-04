b19b520b3177dec024aa62b033b8106771405b31
Miguelitov1 <miguelvisintini1@gmail.com>
2023-12-04 09:10:07 +0100

    maquetacion pagina buscador

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,58 @@
-
+import { HeroCard } from "../components/index";
 
 export const SearchPage = () => {
   return (
-    <h1>SearchPage</h1>
+    <>
+
+      <h1>Search</h1>
+      <hr />
+
+      <div className="row">
+
+        <div className="col-3">
+          
+          <h4>Searching</h4>
+          <hr />
+
+          <form>
+            <input 
+              type="text" 
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+            />
+
+            <button
+              className="btn btn-outline-primary mt-1">
+              Search
+            </button>
+
+          </form>
+
+        </div>
+
+        <div className="col-9">
+          
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>          
+          
+          <div className="alert alert-danger">
+            There's no hero with <b>ABC</b>
+          </div>
+
+          <HeroCard />
+
+        </div>
+
+      </div>
+
+
+    </>
+
   )
 }
